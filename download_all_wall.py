97690b37bb2b5e8a8aa802066963be5b07679c89
Amit Bhandari <amit@ipropal.com>
2018-02-14 15:37:48 +0530

    invalid jpegs will be deleted

--- download_all_wall.py
+++ download_all_wall.py
@@ -28,7 +28,7 @@ SEARCH_NAME = SEARCH_NAME.replace(' ','+')
 SEARCH_URL = "https://www.google.co.in/search?q={}&source=lnms&tbm=isch&tbs=isz:ex,iszw:1920,iszh:1080".format(SEARCH_NAME)
 print SEARCH_URL
 
-DOWNLOAD_PATH = "F:\\remote\\wallpaper\\{}\\".format(sys.argv[1])
+DOWNLOAD_PATH = "wallpaper/{}/".format(sys.argv[1])
 if not os.path.exists(DOWNLOAD_PATH):
     os.makedirs(DOWNLOAD_PATH)
 
@@ -36,7 +36,7 @@ if not os.path.exists(DOWNLOAD_PATH):
 #manipulate user agent so as to make them believe we are just normal human downloading some image
 hdr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-#make a request and get html in beautiful suop
+#make a request and get html in beautiful soup
 req = urllib2.Request(SEARCH_URL,headers=hdr)
 page = urllib2.urlopen(req)
 soup = BeautifulSoup(page, 'html.parser')
@@ -45,7 +45,9 @@ soup = BeautifulSoup(page, 'html.parser')
 
 #scrap the html to find link of image (just refer to html printed in above prettify line to find what html google returns)
 #google image will give us total 99 images (I think), just pick one randomly. And hope for the best.
+
 invalid_jpeg = True
+
 for i in range(99):
 
 	#path for storing the photo
@@ -56,17 +58,16 @@ for i in range(99):
 	    LINK = data_content["ou"]
 	     #dowload the photo 
 	    print LINK
-	 	 
 	 	
 	res = requests.get(LINK, headers=hdr)
 	with open(PHOTO_PATH, 'wb') as W:
 		W.write(res.content)
-	    
-	#be nice to google    
-	time.sleep(1.5)
+		
+	if(imghdr.what(PHOTO_PATH)!="jpeg"):
+		#delete the file
+		os.remove(PHOTO_PATH)
+		    
 
+	
 
-#set as wallpaper
-#commands works for unity in my Ubuntu 16.04, might be different for your desktop. Just google it. P.S. Difficult if you are using KDE
-#@todo command for changing windows wallpaper
 
